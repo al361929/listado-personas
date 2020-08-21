e210d01e0c8a13025883b73df190945508cb5974
al361929 <al361929@uji.es>
2020-08-21 18:47:40 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Juarez'),
+  ];
+  nombreInput:string;
+  apellidoInput:string;
+
+  onAgregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
 }
---
+++ src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Persona { //NOTE export sirve para que podamos usar la clase en otros ficheros
+  // nombre:string;
+  // apellido:string;
+
+  // constructor(nombre:string, apellido:string){
+  //   this.nombre = nombre;
+  //   this.apellido = apellido;
+  // }
+  // NOTE esta sintaxis es más extensa, la siguiente es más simplificada
+
+  constructor(public nombre: string, public apellido: string) { } //NOTE al añadir public estamos creando los atributos y asignando los valores desde este constructor
+
+}
