573cd8ba13103e9d588fd8d3b0855c7629d2b531
al361929 <al361929@uji.es>
2020-08-21 17:18:07 +0200

    prueba2

--- src/app/persona.model.ts
+++ src/app/persona.model.ts
@@ -8,6 +8,7 @@ export class Persona { //NOTE export sirve para que podamos usar la clase en otr
   // }
   // NOTE esta sintaxis es más extensa, la siguiente es más simplificada
   // NOTE PRUEBA DEL GIT HUB
+  // NOTE PRUEBA 2
 
   constructor(public nombre: string, public apellido: string) { } //NOTE al añadir public estamos creando los atributos y asignando los valores desde este constructor
 
