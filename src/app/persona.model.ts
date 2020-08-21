8b177dc724b40b83415c493fd6ac8faf118a130b
al361929 <al361929@uji.es>
2020-08-21 17:25:16 +0200

    ara si que si

--- src/app/persona.model.ts
+++ src/app/persona.model.ts
@@ -7,11 +7,6 @@ export class Persona { //NOTE export sirve para que podamos usar la clase en otr
   //   this.apellido = apellido;
   // }
   // NOTE esta sintaxis es más extensa, la siguiente es más simplificada
-  // NOTE PRUEBA DEL GIT HUB
-  // NOTE PRUEBA 2
-  // NOTE PRUEBA 3
-  // NOTE PRUEBA 5
-  // NOTE PRUEBA 6
 
   constructor(public nombre: string, public apellido: string) { } //NOTE al añadir public estamos creando los atributos y asignando los valores desde este constructor
 
