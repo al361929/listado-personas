a7f0cbb3d43b5b3389aad749bcc45ca2d0b2e41f
al361929 <al361929@uji.es>
2020-08-24 19:24:34 +0200

    clase 29

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -9,8 +9,9 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class FormularioComponent implements OnInit {
   @Output() personaCreada = new EventEmitter<Persona>(); // NOTE De esta manera estamos creando un nuevo evento para avisar a la clase padre desde la clase hijo
   // NOTE Con esta sintaxis hemos creado un objeto EventEmitter de tipo Persona que posteriormente emitirá un evento
-  nombreInput:string;
-  apellidoInput:string;
+  //nombreInput:string;
+  //apellidoInput:string;
+  // NOTE Se comenta porque hemos pasado a utilizar variables locales del modelo html
 
   constructor() {
 
@@ -19,8 +20,8 @@ export class FormularioComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAgregarPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+  onAgregarPersona(nombreInput:HTMLInputElement, apellidoInput:HTMLInputElement){ // NOTE Como utilizamos una variable local en el modelo, lo que se le pasa al método es un objeto de tipo HTMLInputElement
+    let persona1 = new Persona(nombreInput.value, apellidoInput.value); // NOTE Mediante el atributo value obtenemos el valor de la variable
     this.personaCreada.emit(persona1);
   }
 
